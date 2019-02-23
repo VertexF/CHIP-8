458fa0e7d934a1e46ef73101dc5424a0d3ae604b
VertexF <danmanley93@gmail.com>
2019-02-23 14:37:03 +0000

    Added the RAM and internals
    Here I added the RAM of the CHIP-8 and the internals. It's done with a union which means you can overwrite the internals if you wanted to.

--- CHIP-8/CHIP-8.cpp
+++ CHIP-8/CHIP-8.cpp
@@ -3,6 +3,24 @@
 
 #include <SDL.h>
 
+static constexpr const unsigned W = 64, H = 32;
+
+struct Chip8
+{
+	union
+	{
+		//Chip-8 has 0x1000 of RAM.
+		unsigned char Mem[0x1000] = { 0 };
+		//Up to 0x200 of the RAM is simulated internals.
+		struct
+		{
+			unsigned char V[16], DelayTimer, SoundTimer, SP, Keys[16];
+			unsigned char DispMem[W * H / 8], Font[16 * 5]; //Monochrome
+			unsigned short PC, Stack[12], I;
+		};
+	};
+};
+
 int main(int argv, char** argc)
 {
 	if(SDL_Init(SDL_INIT_EVERYTHING) < 0)
