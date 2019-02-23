d843418c760064c53f10480621b4fda642bc7d61
VertexF <danmanley93@gmail.com>
2019-02-23 14:22:51 +0000

    SDL set up
    I am just setting up the SDL in the main to make sure that Visual Studios linked things correctly.

--- CHIP-8/CHIP-8.cpp
+++ CHIP-8/CHIP-8.cpp
@@ -1,9 +1,17 @@
 #include "pch.h"
 #include <iostream>
 
-int main()
+#include <SDL.h>
+
+int main(int argv, char** argc)
 {
+	if(SDL_Init(SDL_INIT_EVERYTHING) < 0)
+	{
+		std::cerr << "SDL failed to start" << std::endl;
+	}
+
     std::cout << "Hello World!\n"; 
+	return 0;
 }
 
 
