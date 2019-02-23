9d61364406aaf94b396a513e4be0ff8a08f90dda
VertexF <danmanley93@gmail.com>
2019-02-23 19:40:27 +0000

    Added all CPU routines
    I have added all the CPU routines in a define. I had to do it all in one go.

--- CHIP-8/CHIP-8.cpp
+++ CHIP-8/CHIP-8.cpp
@@ -1,5 +1,6 @@
 #include "pch.h"
 #include <iostream>
+#include <random>
 
 #include <SDL.h>
 
@@ -20,6 +21,9 @@ struct Chip8
 		};
 	};
 
+	//The CPU contains some RND stuff.
+	std::mt19937 rnd{};
+
 	//The initialisation
 	Chip8()
 	{
@@ -33,6 +37,49 @@ struct Chip8
 		}
 	}
 
+	#define LIST_INSTRUCTIONS(o) \
+		o("cls",			"00E0",	for(auto& c: DispMem) c = 0		)\
+		o("ret",			"00EE",	PC = Stack[SP-- % 12]			)\
+		o("jp N",			"1nnn",	PC = nnn						)\
+		o("jp v0,N",		"Bnnn",	PC = nnn + V[0]					)\
+		o("call N",			"2nnn",	Stack[++SP % 12] = 12; PC = nnn	)\
+		o("ld vX,k",		"Fx0A",	/*WaitingKey = 0x80 | x*/		)\
+		o("ld vX,K",		"6xkk",	Vx = kk							)\
+		o("ld vX,vY",		"8xy0",	Vx = Vy							)\
+		o("add vX,K",		"7xkk",	Vx += kk						)\
+		o("or vX,vY",		"8xy1",	Vx |= Vy						)\
+		o("and vX,vY",		"8xy2",	Vx &= Vy						)\
+		o("xor vX,vY",		"8xy3",	Vx ^= Vy						)\
+		o("add vX,vY",		"8xy4",	p = Vx+Vy; VF = (p>>8); Vx = p	)\
+		o("sub vX,vY",		"8xy5",	p = Vx-Vy; VF = !(p>>8); Vx = p	)\
+		o("subn vX,vY",		"8xy7",	p = Vy-Vx; VF = !(p>>8); Vx = p	)\
+		o("shr vX",			"8xy6",	VF = Vy & 1; Vx = Vy >> 1		)\
+		o("shl vX",			"8xyE",	VF = Vy >> 7; Vx = Vy << 1		)\
+		o("rnd vX,K",		"Cxkk",	\
+		Vx = std::uniform_int_distribution<>(0,255)(rnd) & kk		)\
+		o("drw vX,vY,P",	"Dxy1",	\
+			auto put = [this](int a, unsigned char b) { return ((DispMem[a] ^= b) ^ b) & b;}; \
+			for(kk=0, x=Vx, y=Vy, p--;)	\
+				kk |= put( ((x+0)%W + (y+p)%H * W) / 8, Mem[(I+p)&0xFFF] >> (x%8)) \
+				kk | put( ((x+7)%W + (y+p)%H * W) / 8, Mem[(I+p)&0xFFF] << (8 - x%8)) )\
+		o("ld f,vX",		"Fx29",	I = &Font[(Vx&15)*5] - Mem		)\
+		o("ld vX,dt",		"Fx07",	Vx = DelayTimer					)\
+		o("ld dt,vX",		"Fx15",	DelayTimer = Vx					)\
+		o("ld st,vX",		"Fx18",	SoundTimer = Vx					)\
+		o("ld i,N",			"Annn",	I = nnn							)\
+		o("add i,vX",		"Fx1E",	p = (I&0xFFF)+Vx; VF=p>>12; I=p	)\
+		o("se vX,K",		"3xkk",	if(kk == Vx) PC += 2			)\
+		o("se vX,vY",		"5xy0",	if(Vy == Vx) PC += 2			)\
+		o("sne vX,K",		"4xkk",	if(kk != Vx) PC += 2			)\
+		o("sne vX,vY",		"9xy0",	if(Vy != Vx) PC += 2			)\
+		o("skp vX",			"Ex9E",	if(Keys[Vx&15]) PC += 2			)\
+		o("sknp vX",		"ExA1",	if(!Keys[Vx&15]) PC += 2		)\
+		o("ld b,vX",		"Fx33",	Mem[(I+0)&0xFFF] = (Vx/100)%10; \
+									Mem[(I+1)&0xFFF] = (Vx/10)%10; \
+									Mem[(I+2)&0xFFF] = (Vx/1)%10	)\
+		o("ld [i],vX",		"Fx55",	for(p=0;p<x;p++) Mem[I++ & 0xFFF]=V[p])\
+		o("ld vX,[i]",		"Fx65",	for(p=0;p<x;p++) V[p]=Mem[I++ & 0xFFF])\
+
 	void ExecIns() 
 	{
 		//Here we are reading in the 2 byte opcode from memory and advanced the program counter.
@@ -43,6 +90,8 @@ struct Chip8
 		unsigned x = (opcode >> 8) & 0xF;
 		unsigned kk = (opcode >> 0) & 0xFF;
 		unsigned nnn = (opcode >> 0) & 0xFFF;
+
+		auto& Vx = V[x], &Vy = V[y], &VF = V[0xF];
 	}
 };
 
