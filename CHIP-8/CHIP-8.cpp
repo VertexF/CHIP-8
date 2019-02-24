1ca31982f3b17a66c015cd5c32206cfaec65b439
VertexF <danmanley93@gmail.com>
2019-02-24 06:51:42 +0000

    Keyboard layout mapped.
    Here I have created a mapping from SDL keys to the Chip-8 keyboard layout.

--- CHIP-8/CHIP-8.cpp
+++ CHIP-8/CHIP-8.cpp
@@ -133,6 +133,17 @@ int main(int argc, char** argv)
 	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
 	SDL_Texture* texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, W, H);
 
+	//Here I have created a mapping from SDL keys to the Chip-8 keyboard layout.
+	std::unordered_map<int, int> keymap
+	{
+		{SDLK_1, 0x1}, {SDLK_2, 0x2}, {SDLK_3, 0x3}, {SDLK_4, 0xC},
+		{SDLK_q, 0x4}, {SDLK_w, 0x5}, {SDLK_e, 0x6}, {SDLK_r, 0xD},
+		{SDLK_a, 0x7}, {SDLK_s, 0x8}, {SDLK_d, 0x9}, {SDLK_f, 0xE},
+		{SDLK_z, 0xA}, {SDLK_x, 0x0}, {SDLK_c, 0xB}, {SDLK_v, 0xF},
+		{SDLK_5, 0x5}, {SDLK_6, 0x6}, {SDLK_7, 0x7}, 
+		{SDLK_1, 0x1}, {SDLK_8, 0x8}, {SDLK_9, 0x9}, {SDLK_ESCAPE, -1}
+	};
+
 	bool interrupted = false;
 	while (!interrupted) 
 	{
